ead90ec72843dc77b755cebff57812a7ad364ddc
Michela30 <128288972+Michela30@users.noreply.github.com>
2023-07-13 14:07:22 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+//import
+
+   export default {
+    data() {
+      return{
+
+      }
+    },
+    components: {
+     
+    },
+  }
+</script>
+
+
+
+<template>
+  <div>
+      <header>
+        HEADER
+      </header>
+
+      <main>
+        MAIN
+      </main>
+
+      <footer>
+        FOOTER
+      </footer>
+  </div>
+</template>
+
+
+
+
+<style lang="scss">
+@use 'assets/scss/main.scss';
+
+
+
+
+</style>
